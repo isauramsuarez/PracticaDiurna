5f7d94d9d0fe2d80dfbb91a3932aa8303a52b2e5
isauramsuarez <isaurasuarez3@gmail.com>
2022-10-11 06:44:52 -0500

    menu principal

---
+++ index.py
@@ -0,0 +1,8 @@
+#código de isaura, lo tiene en el gibhub del lider en la rama master
+#menu de opciones
+
+print("Estilos y Belleza")
+print("1. ver informacion de la empresa")
+print("2. Comprar")
+print("3. Agendar cita")
+opcion=int(input("digite opcion: "))
